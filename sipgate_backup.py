6b24fb3df97005242ca36a3935d27497d813cf97
pkoevesdi <33196971+pkoevesdi@users.noreply.github.com>
2018-02-15 09:47:28 +0100

    Update sipgate_backup.py
    minor changes to filepath operations

--- sipgate_backup.py
+++ sipgate_backup.py
@@ -2,10 +2,13 @@ import csv, json, requests, os, wget, urllib
 
 user="w0" # can be retrieved from sipgate URL, i.e. last two letters of https://secure.live.sipgate.de/settings/phone/index/webuser/1234567w0
 limit=100 # max. items per call, max. 1000 (limited by sipgate api)
-fileloc='/home/gauner/Downloads/any_prefix '+user+' any_suffix/' # local filepath, for windows use "c:\\users\\whatever\\" form
+dir_to_save_to='any prefix '+user+' any suffix' # Subdirectory (from this script's location) to save to
 username='...' # Sipgate username
 password='...'# Sipgate password
 
+# for just using this script, don't change below this line
+
+fileloc=(os.path.join(os.path.dirname(os.path.realpath(__file__)),dir_to_save_to ))	# local filepath
 url = 'https://api.sipgate.com/v1/'+user+'/history'
 data=[]
 header={}
@@ -32,7 +35,7 @@ print("Retrieved "+str(j)+" items.")
 if not os.path.exists(fileloc):
     os.makedirs(fileloc)
 
-with open(fileloc+user+'.csv', 'w') as csvfile:
+with open(os.path.join(fileloc,user+'.csv'), 'w') as csvfile:
 	output = csv.DictWriter(csvfile, header.keys(), lineterminator="\n")
 	output.writeheader()
 
@@ -41,13 +44,14 @@ with open(fileloc+user+'.csv', 'w') as csvfile:
 		row.update(dict.fromkeys(set(header.keys()).difference(row), ''))
 		print('writing csv line for item',str(i)+":",row)
 		output.writerow(row)
-		for item in {k: v for k, v in header.items() if "url" in k.lower()}.keys():
-			filename=os.path.basename(urllib.parse.urlparse(row[item]).path)
-			if row[item]:
-				filepath=fileloc+user+'_'+row["id"]+'_'+item.lower().replace('url','')+'_'+filename
-				if not os.path.isfile(filepath):
-					print ("retrieving file for item",str(i)+":",row[item])
-					wget.download(row[item], filepath) 
-				else:
-					print ('file for item',str(i)+" exists:",filepath)
+		for item in header.keys():
+			if "url" in item.lower():
+				filename=os.path.basename(urllib.parse.urlparse(row[item]).path)
+				if row[item]:
+					filepath=os.path.join(fileloc,user+'_'+row["id"]+'_'+item.lower().replace('url','')+'_'+filename)
+					if not os.path.isfile(filepath):
+						print ("retrieving file for item",str(i)+":",row[item])
+						wget.download(row[item], filepath) 
+					else:
+						print ('file for item',str(i)+" exists:",filepath)
 		i+=1
